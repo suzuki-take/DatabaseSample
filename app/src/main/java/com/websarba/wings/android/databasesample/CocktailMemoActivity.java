59da2b98f0616181df30eee69db064f29be2080b
suzuki <taketoshi.rog@gmail.com>
2020-08-25 08:58:28 +0900

    Initial commit

---
+++ app/src/main/java/com/websarba/wings/android/databasesample/CocktailMemoActivity.java
@@ -0,0 +1,14 @@
+package com.websarba.wings.android.databasesample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CocktailMemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cocktail_memo);
+    }
+}
